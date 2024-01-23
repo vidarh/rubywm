d4fdee3b578d7bdfd407a46e270aea045405dd9a
Vidar Hokstad <vidar@hokstad.com>
2024-01-23 01:51:32 +0000

    Monkey patch cleaned up Geometry inspect to ease debugging.

--- geom.rb
+++ geom.rb
@@ -1,4 +1,11 @@
 
+module X11
+  module Form
+    class Geometry
+      def inspect = "<Geometry x=#{x.to_i} y=#{y.to_i} width=#{width.to_i} height=#{height.to_i}>"
+    end
+  end
+end
 def gap(geom,g)
   geom = geom.dup
   geom.x += g
